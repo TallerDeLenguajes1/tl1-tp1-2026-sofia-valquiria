23cb2ef1089540a3a23ee8cc328ab0eaae3c9695
PlataIvan <ivanplataunt@gmail.com>
2026-04-07 16:35:04 -0300

    duda en linea 11 de tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    printf("(ya estoy HARTA de escrubir tantos) hola mundo");
+    printf("(ya estoy HARTA de escrubir tantos) hola mundo\n");
+    
+    double n = 2048*2048;
+    double* pN = &n;
+    printf("Contenido de puntero: %p \n", *pN);
+    printf("Contenido de la variable: %p \n", pN);
+    printf("Direccion de memoria de la variable: %p \n", &n);
+    printf("Direccion de memoria del puntero pN apuntado a n (SUPUESTAMENTE): %p \n", &pN); //AYUDA, ESTI DEBERUA SER DISTINTO A LAS DOS DE ARRIBA PERO ES IGUAL AAAAAAAAAAAAAAAAAAAAAA    
+    printf("El tamaño de memoria utilizado es: %d bytes", sizeof(n)); 
     return 0;
 }
